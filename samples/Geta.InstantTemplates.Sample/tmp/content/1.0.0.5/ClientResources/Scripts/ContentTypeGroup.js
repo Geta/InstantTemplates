f937e18480088a0243421d12995acf4ab94e3609
milosmih92 <45643734+milosmih92@users.noreply.github.com>
2019-08-27 02:38:58 +0200

    Added NuGet package creation.

---
+++ samples/Geta.InstantTemplates.Sample/tmp/content/1.0.0.5/ClientResources/Scripts/ContentTypeGroup.js
@@ -0,0 +1,149 @@
+﻿// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/_base/array",
+    "dojo/dom-style",
+    "dojo/text!./templates/ContentTypeGroup.html",
+    "dojo/topic",
+    "dojo",
+
+    "dijit/_TemplatedMixin",
+    "dijit/layout/_LayoutWidget",
+
+    "./ContentType",
+    "dojo/keys",
+    "dijit/_KeyNavContainer",
+
+    "epi/dependency"
+
+], function (declare, lang, array, domStyle, template, topic, dojo, _TemplatedMixin, _LayoutWidget, ContentType, keys, _KeyNavContainer, dependency) {
+
+    return declare([_LayoutWidget, _TemplatedMixin, _KeyNavContainer], {
+        // summary:
+        //		Displays a group of content types under a common heading.
+        //
+        // tags:
+        //      internal
+
+        // title: [public] String
+        //		The title for the content type group.
+        title: "",
+
+        // contentTypes: [public] Array
+        //		Collection of content types that are displayed in the group.
+        contentTypes: null,
+
+        // templateString: [protected] String
+        //		A string that represents the widget template.
+        templateString: template,
+
+        contentDataStore: null,
+
+        templatesRoot: null,
+
+        buildRendering: function () {
+            // summary:
+            //		Construct the UI with the initial content types
+            // tags:
+            //		protected
+
+            this.inherited(arguments);
+        },
+
+        postCreate: function () {
+            this.inherited(arguments);
+
+            this.render();
+
+            this.connectKeyNavHandlers(
+				this.isLeftToRight() ? [keys.LEFT_ARROW, keys.UP_ARROW] : [keys.RIGHT_ARROW, keys.DOWN_ARROW],
+				this.isLeftToRight() ? [keys.RIGHT_ARROW, keys.DOWN_ARROW] : [keys.LEFT_ARROW, keys.UP_ARROW]
+			);
+        },
+
+        render: function () {
+            // summary:
+            //		Render the group with the current content types. This will
+            //		destroy the current view if it exists.
+            // tags:
+            //		public
+
+            this.clear();
+            var that = this;
+
+            var registry = dependency.resolve("epi.storeregistry");
+
+            var instantTemplatesStore = registry.get("instanttemplates");
+
+            dojo.when(instantTemplatesStore.get(that.parentLink), function (response) {
+                array.forEach(response, function (contentData) {
+                    var child = new ContentType({ contentData: contentData });
+                    this.connect(child, "onSelect", this.onSelect);
+                    this.addChild(child);
+                }, that);
+            });
+        },
+
+        clear: function () {
+            // summary:
+            //		Destroys the current view.
+            // tags:
+            //		public
+
+            array.forEach(this.getChildren(), function (child) {
+                this.removeChild(child);
+                child.destroyRecursive();
+            }, this);
+        },
+
+        _setTitleAttr: { node: "titleNode", type: "innerText" },
+
+        _setContentTypesAttr: function (value) {
+            this._set("contentTypes", value);
+            if (this._created) {
+                this.render();
+            }
+        },
+
+        onSelect: function (item) {
+            // summary:
+            //		Callback that is executed when an item in this
+            //		group is selected.
+            // tags:
+            //		callback
+
+            topic.publish("/epi/shell/action/changeview", "instantTemplates/CreateContentView", null, {
+                parent: this.parentLink,
+                contentLink: item.contentLink,
+                headingText: "New Instant Template",
+                templateName: item.name
+            });
+        },
+
+        setVisibility: function (display) {
+            // summary:
+            //    Set the group's visibility
+            //
+            // display:
+            //    Flag states if the group will be shown or not.
+            //
+            // tags:
+            //    public
+            var value = (display ? "block" : "none");
+            domStyle.set(this.domNode, "display", value);
+        },
+        getContentDataStore: function () {
+            // summary:
+            //      Gets the content data store from store registry if it's not already cached
+
+            if (!this.contentDataStore) {
+                var registry = dependency.resolve("epi.storeregistry");
+                this.contentDataStore = registry.get("epi.cms.content.light");
+            }
+            return this.contentDataStore;
+        },
+    });
+});
---
+++ src/tmp/content/1.0.0.5/ClientResources/Scripts/ContentTypeList.js
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+define([
+// dojo
+    "dojo/_base/array",
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+
+    "dojo/dom-class",
+
+    "dojo/Deferred",
+    "dojo/promise/all",
+    "dojo/when",
+    "dojo/topic",
+
+// dijit
+    "dijit/layout/_LayoutWidget",
+// epi
+    "epi/dependency",
+    "epi/shell/TypeDescriptorManager",
+    "./ContentTypeGroup"
+],
+
+function (
+// dojo
+    array,
+    declare,
+    lang,
+
+    domClass,
+
+    Deferred,
+    all,
+    when,
+    topic,
+// dijit
+    _LayoutWidget,
+// epi
+    dependency,
+    TypeDescriptorManager,
+    ContentTypeGroup
+) {
+
+    return declare([_LayoutWidget], {
+        // summary:
+        //      A list of suggested and available content types for content creation.
+        // description:
+        //      Displays a list of suggested and available content types for content creation.
+        // tags:
+        //      internal
+
+        // parentLink: [public] String
+        //      Link to parent content which the new content will be created beneath.
+        parentLink: null,
+
+        // contentLink: [public] String
+        //     The contentlink for which the list will be filtered on.
+        contentLink: null,
+
+        templatesRoot: null,
+
+        buildRendering: function () {
+            // summary:
+            //      Construct the base UI with suggested content types.
+            // tags:
+            //      protected
+
+            this.inherited(arguments);
+
+            var suggested = new ContentTypeGroup({templatesRoot: this.templatesRoot, parentLink: this.parentLink});
+
+            domClass.add(suggested.titleNode, "epi-ribbonHeaderSpecial");
+            suggested.set("title", "Available Instant Templates");
+            suggested.set("templatesRoot", this.templatesRoot);
+            suggested.setVisibility(true);
+            this.addChild(suggested);
+            this._suggestedContentTypes = suggested;
+
+            this.set("shouldSkipContentTypeSelection", false);
+        },
+    });
+
+});
